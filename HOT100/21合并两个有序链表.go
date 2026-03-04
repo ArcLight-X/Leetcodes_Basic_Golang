b76921428c32dc9e255661835230d13d69ae096a
ArcLight-X <18512182192@163.com>
2026-03-04 17:02:30 +0800

    Add files via upload

---
+++ HOT100/21合并两个有序链表.go
@@ -0,0 +1,28 @@
+package main
+
+import("fmt"
+
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	//一个链表为空就不用合并
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val <= list2.Val { //比较两个链表头节点
+		list1.Next = mergeTwoLists(list1.Next, list2) //把较小的那个头节点排在最前，剩下还是合并有序链表
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
